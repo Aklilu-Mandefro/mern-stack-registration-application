2a1062cfd2e7f10500692c214f7200b22fb92cc5
Aklilu Mandefro <aklilu.mandefro@volunteer-tech.com>
2023-07-01 14:07:05 -0700

    main component and style files added

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './router/AppRouter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.scss';
+
+ReactDOM.render(<AppRouter />, document.getElementById('root'));
